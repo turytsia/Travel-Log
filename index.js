e7ff114660ca5bb127543c3429341ccc0586c393
Alexander <70067346+rosjy@users.noreply.github.com>
2021-11-14 18:27:40 +0100

    1.0

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ app.use(
         credentials: true,
     })
 );
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.use('/api/image', express.static('images'));
 app.use(express.json());
 app.use(cookieParser());
